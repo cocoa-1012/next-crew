d140e0a31b8af5a3584c0224dda1e2b59a640db9
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-06 05:09:39 -0400

    update04

--- components/attacksMain.js
+++ components/attacksMain.js
@@ -62,7 +62,7 @@ export default function AttacksMain() {
         <div className="flex flex-row justify-between items-start px-12 py-12 my-4 bg-white rounded shadow">
           <p className="rounded text-gray-700 flex-1">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industry`s standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
