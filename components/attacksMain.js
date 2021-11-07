6adb34f31a9cba1ab7334924cef358cb92beccd3
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-07 14:52:30 -0500

    add label on attacks table

--- components/attacksMain.js
+++ components/attacksMain.js
@@ -66,15 +66,14 @@ export default function AttacksMain() {
             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
             nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            esse cillum dolore eu fugiat nulla pariatur.
           </p>
 
           <AttacksModal />
         </div>
-        <div className="grid grid-cols-1 gap-8 mt-4">
+        <div className="grid grid-cols-1 mt-4">
           <div className=" h-auto ">
+            <p className="font-bold text-gray-500 mx-8 p-2">Attacks</p>
             <AttackTable data={Attacks} />
           </div>
         </div>
