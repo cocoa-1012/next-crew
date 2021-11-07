9b8a06c756c3363b7d7bcea72487368fec0d6f78
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-11-07 12:37:20 -0500

    modal and pagination update

--- components/attacksMain.js
+++ components/attacksMain.js
@@ -1,5 +1,8 @@
 // import People1 from "../const/tableData";
+import React from "react";
+import AttacksModal from "./attacksModal";
 import AttackTable from "./attackTable";
+import PaginationSection from "./paginationSection";
 const Attacks = [
   {
     name: "Attack 1",
@@ -67,182 +70,15 @@ export default function AttacksMain() {
             cupidatat non proident, sunt in culpa qui officia deserunt mollit
             anim id est laborum.
           </p>
-          <button className="ml-16 bg-purple-700 rounded px-8 py-3 text-white font-bold uppercase hover:bg-purple-500">
-            New Attack
-          </button>
+
+          <AttacksModal />
         </div>
         <div className="grid grid-cols-1 gap-8 mt-4">
           <div className=" h-auto ">
             <AttackTable data={Attacks} />
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-4 mt-4">
-          <div className="w-full h-auto">
-            <link
-              rel="stylesheet"
-              href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
-            />
-
-            <div className="flex items-center justify-center mb-4">
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  rounded-l
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                <i className="fas fa-angle-double-left"></i>
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                <i className="fas fa-angle-left"></i>
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                1
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                2
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                3
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                <i className="fas fa-angle-right"></i>
-              </button>
-              <button
-                className="
-                  bg-purple-500
-                  text-white
-                  hover:bg-purple-700 hover:text-white
-                  active:bg-purple-700
-                  font-bold
-                  uppercase
-                  text-xs
-                  px-4
-                  py-2
-                  rounded-r
-                  outline-none
-                  focus:outline-none
-                  mb-1
-                  ease-linear
-                  transition-all
-                  duration-150
-                "
-                type="button"
-              >
-                <i className="fas fa-angle-double-right"></i>
-              </button>
-            </div>
-          </div>
-        </div>
+        <PaginationSection />
       </div>
     </div>
   );
